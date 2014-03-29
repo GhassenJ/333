aae2bf2ca1ef0544dd501dc80b4dd9eedd919b11
Matthew Matl <mmatl@princeton.edu>
2014-03-28 20:02:58 -0400

    Updated models

--- princeton-marketplace/market/models.py
+++ princeton-marketplace/market/models.py
@@ -8,6 +8,14 @@ from django.utils import timezone
 # Control-defined categories for goods and services
 class Category(models.Model):
     name = models.CharField(max_length = 200)
+    num_posts = models.IntegerField(default = 0)
+    def __unicode__(self):
+        return self.name
+
+# Hashtag categorization
+class Hashtag(models.Model):
+    name = models.CharField(max_length = 200)
+    frequency = models.IntegerField(default = 0)
     def __unicode__(self):
         return self.name
 
@@ -17,22 +25,40 @@ class UserProfile(models.Model):
     phone_no = models.CharField(max_length = 20)
     class_year = models.IntegerField(default = 2000)
     categories = models.ManyToManyField(Category) # Link to desired categories
+    hashtags = models.ManyToManyField(Hashtag, blank=True, null=True) # Link to hashtags used
+    rating = models.IntegerField(default = 0)
     transactions = models.IntegerField(default = 0)
     def __unicode__(self):
         return self.user.username
 
+# User Reviews
+class Review(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 4000)
+    date_posted = models.DateTimeField('Date Posted', default = timezone.now())
+    rating = models.IntegerField(default = 5)
+    author = models.ForeignKey(User, related_name='review_author')
+    reviewee = models.ForeignKey(User, related_name='review_reviewee')
+
 # A buying or selling post
 class Posting(models.Model):
     title = models.CharField(max_length = 200)
+    
     author = models.ForeignKey(User, related_name='author') # Link to user author
-    is_open = models.BooleanField(default = True)
-    responder = models.ManyToManyField(User, related_name='responder') # Link to user responders
+    responder = models.ManyToManyField(User, related_name='responder', blank=True, null=True) # Link to user responders
+    
     date_posted = models.DateTimeField('Date Posted', default = timezone.now())
     date_expires = models.DateTimeField('Expiration Date', default = timezone.now())
+    
     method_of_pay = models.CharField(max_length = 200)
-    category = models.ForeignKey(Category) # Link to relevant category
-    description = models.CharField(max_length = 2000)
     price = models.CharField(max_length = 50)
+
+    description = models.CharField(max_length = 2000)
+    
     is_selling = models.BooleanField(default = True)
+    is_open = models.BooleanField(default = True)
+
+    category = models.ForeignKey(Category) # Link to relevant category
+    hashtags = models.ManyToManyField(Hashtag, blank=True, null=True)
     def __unicode__(self):
         return self.title
