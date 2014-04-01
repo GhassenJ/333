f536aff831317c2e32eb46342f6af67c49907ca9
Ghastn <ghassouna@gmail.com>
2014-04-01 03:46:53 -0400

    jQuery

---
+++ jQuery Frontend/Readme.txt
@@ -0,0 +1,6 @@
+This is a jQuery attempt at our Ajax rendering of the postings.
+This requires setting up the node.Js server which can be done by calling:
+npm install
+and then calling:
+node app.js
+After this, go on to localhost:8000/index.html and you will find a functionnal app
---
+++ jQuery Frontend/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.send('Hello World\n');
+});
+app.get('/index.html', function(req, res) {
+    res.sendfile('public/index_Json_parse.html', {root: __dirname })
+});
+app.get('/data.json', function(req, res) {
+    res.sendfile('public/data.json', {root: __dirname })
+});
+app.get('/data2.json', function(req, res) {
+    res.sendfile('public/data2.json', {root: __dirname })
+});
+app.listen(8000);
