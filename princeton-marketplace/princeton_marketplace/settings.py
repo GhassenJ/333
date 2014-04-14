c45388b2172e40c872392790e4580d8d836f2478
Ghastn <ghassouna@gmail.com>
2014-04-14 14:04:46 -0400

    fixed stuff

--- princeton-marketplace/princeton_marketplace/settings.py
+++ princeton-marketplace/princeton_marketplace/settings.py
@@ -86,7 +86,7 @@ else:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'market',
             'USER': 'root',
-            'PASSWORD': 'kbs414'
+            'PASSWORD': 'root'
         }
     }
 
