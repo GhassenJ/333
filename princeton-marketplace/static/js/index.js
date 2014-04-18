2c5e69ec205cfd9754a68bd2356511d5bbcc7f8e
Parth Mehta <pcmehta257@PCM-Laptop.(none)>
2014-04-18 07:44:10 -0400

    need to sleep now, really

--- princeton-marketplace/static/js/index.js
+++ princeton-marketplace/static/js/index.js
@@ -1,9 +1,13 @@
 $(document).ready(main);
 
 // GLOBAL VARIABLES
-var lastDisplay = $("div.home_feed");
+var last_url = "home_feed";
+var current_url = "home_feed";
+var user_id;
 
 // MOUSE-CLICK HANDLERS
+
+// handles click on any menu item on the leftPane
 function leftPaneHandler(e){
   var url = this.className.split(/\s+/)[1];
   var template = $("script." + url).html();
@@ -13,19 +17,52 @@ function leftPaneHandler(e){
     for (var i = 0; i < len; i++){
       var ready = Mustache.render(template, data[i]);
       $("tbody." + url).append(ready);
+      $("button#post-" + data[i]["id"]).click(openPostHandler);
     }
-    lastDisplay.hide();
-    lastDisplay = $("div." + url);
-    lastDisplay.show();
+    $("div." + current_url).hide();
+    $("div." + url).show();
+    last_url = current_url;
+    current_url = url;
   });
+}
+
+// handles opening of any post for viewing (not editing)
+// this post can be originated by the current user or some other user
+function openPostHandler(e){
+  var url = "posting_detail";
+  var id = this.id.split(/\-+/)[1];
+  // template
+  $.getJSON("/" + url + "/" + id, function (data){
+    var ready = $("div." + url).html();
+    $("div." + url).empty();
+    // var ready = Mustache.render(template, data);
+    $("div." + url).html(ready);
+    $("div." + current_url).hide();
+    $("div." + url).show();
+    last_url = current_url;
+    current_url = url;
+    $("a#back").show();
+  });
+}
+
+// handles clicks on the "back" button in the topBar
+// this button is displayed only sometimes, so you might not see it on the home screen
+function backHandler(e){
+  $("div." + current_url).hide();
+  $("div." + last_url).show();
+  current_url = last_url;
+  last_url = "home_feed";
+  $("a#back").hide();
 }
 
 // THIS IS WHERE EXECUTION STARTS
 function main(){
+  var user_id = parseInt($("script#user_id").html());
   Mustache.tags = ['[[', ']]'];
   var leftPane = $("a").filter(function (){
     return this.className.match(/leftPane/);
   });
   leftPane.click(leftPaneHandler);
+  $("a#back").click(backHandler);
   console.log("done");
 }
